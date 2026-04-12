8f44985a5d233c4d4e0517ab7b17151861d706fc
hrt05 <hirate05program@gmail.com>
2026-04-13 01:38:48 +0900

    表示までできました。

--- src/app/api/gemma/route.ts
+++ src/app/api/gemma/route.ts
@@ -2,16 +2,30 @@ import { NextRequest, NextResponse } from "next/server";
 
 export const POST = async (req: NextRequest) => {
 
-    const reqestBody = await req
+    const reqestBody = await req.json()
+    const parseReqestBody = typeof reqestBody === "string" ? reqestBody : (reqestBody?.prompt ?? reqestBody?.content ?? "")
     const apiEndPoint = process.env.NEXT_PUBLIC_API_URL
+
+    const sendBody = {
+        model: "gemma-7b-it-Q4_K_M.gguf",
+        // messages: [{"role":"system","content":"あなたは日本語で答えるアシスタントです。・事実が不確かなときは推測と明記する。・不要な前置きや自己紹介はしない。・質問に直接答え、必要なら最後に短く補足する。・<|...|> のような特殊な区切り記号や、見出し用の記号だけの行は出力しない。・ユーザーが求めていない「正解」「採点」「修正案の比較」などの形式で答えない。"}, {"role": "user", "content": parseReqestBody}]
+        messages: [{"role":"system","content":"あなたは日本語で答えるアシスタントです。・事実が不確かなときは推測と明記する。・不要な前置きや自己紹介はしない。・質問に直接答え、必要なら最後に短く補足する。・<|...|> のような特殊な区切り記号や、見出し用の記号だけの行は出力しない。・ユーザーが求めていない「正解」「採点」「修正案の比較」などの形式で答えない。"}, {"role": "user", "content": parseReqestBody}]
+    }
 
     const apiFetch = await fetch(`${apiEndPoint}`, {
         method: "POST",
-        body: `${reqestBody}`
+        headers: { "Content-Type": "application/json" },
+        // body: JSON.stringify(reqestBody)
+        body: JSON.stringify(sendBody)
     })
-    if (apiFetch?.ok) {
-        const data = apiFetch
 
-        return NextResponse.json(data)
+    console.log(reqestBody)
+    if (apiFetch?.ok) {
+        const data = await apiFetch.json()
+
+        console.log(data.choices[0].message.content)
+        return NextResponse.json(data.choices[0].message.content)
+    } else {
+        return NextResponse.json("どこかでエラーが起きてます。")
     }
 }
--- src/components/topPageComponent/page.tsx
+++ src/components/topPageComponent/page.tsx
@@ -4,9 +4,22 @@ import { useState } from "react"
 
 const TopPageComponent = () => {
     const [prompt, setPrompt] = useState("")
+    const [gemmaContent, setGemmaContent] = useState([])
 
-    const handleSubmit = () => {
+    const handleSubmit = async () => {
         console.log("押下テスト")
+        const apiFetch = await fetch("/api/gemma", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify(prompt),
+        })
+        if (apiFetch?.ok) {
+            console.log("apiは送信できているかも")
+            // const data = apiFetch
+            // console.log(apiFetch.json())
+            const apiFetchBody = await apiFetch.json()
+            setGemmaContent(apiFetchBody)
+        }
     }
 
     return(
@@ -17,6 +30,10 @@ const TopPageComponent = () => {
             <p>ここにプロンプトを入力してください</p>
             <input placeholder="プロンプト入力" value={prompt} type="text" onChange={(e) => setPrompt(e.target.value)}></input>
             <button onClick={handleSubmit}>送信！</button>
+
+            <div>
+                <p>{gemmaContent}</p>
+            </div>
         </div>
     )
 }
